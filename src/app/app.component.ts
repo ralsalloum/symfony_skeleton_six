ee1632140748733a72c40d7548a71aa0dbc8dc35
ralsalloum <ramisalloum12@hotmail.com>
2022-02-16 21:28:55 +0200

    angular first comment

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const SwaggerUIBundle: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'swagger-ui-dist-angular';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const ui = SwaggerUIBundle({
+      dom_id: '#swagger-ui',
+      layout: 'BaseLayout',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+      ],
+      url: 'https://134.209.241.49/doc.json',
+      docExpansion: 'none',
+      operationsSorter: 'alpha'
+    });
+  }
+}
